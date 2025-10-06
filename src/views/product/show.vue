486dc7973b1841bc4996443753bb6ec45f32e398
tcandra24 <tcandra007@gmail.com>
2025-10-06 16:10:25 +0700

    Update Show Product, Rev Edit Sompany Profile

--- src/views/product/show.vue
+++ src/views/product/show.vue
@@ -64,11 +64,18 @@
             data-aos-duration="1000"
           />
         </div>
-        <div class="py-14 md:py-20">
+        <div class="py-7 md:py-10">
           <h3 class="mb-7 text-xl font-extrabold text-black dark:text-white sm:text-2xl">Penjelasan</h3>
           <p
             class="detail-description relative font-semibold leading-[30px] before:absolute before:top-0 before:h-full before:w-1 before:rounded before:bg-primary ltr:pl-6 ltr:before:left-0 rtl:pr-6 rtl:before:right-0 md:text-lg"
             v-html="product?.description"
+          ></p>
+        </div>
+        <div class="py-7 md:py-10">
+          <h3 class="mb-7 text-xl font-extrabold text-black dark:text-white sm:text-2xl">Keunggulan</h3>
+          <p
+            class="detail-advantage relative font-semibold leading-[30px] text-justify before:absolute before:top-0 before:h-full before:w-1 before:rounded before:bg-secondary ltr:pl-6 ltr:before:left-0 rtl:pr-6 rtl:before:right-0 md:text-lg"
+            v-html="product?.advantage"
           ></p>
         </div>
         <div v-if="product.socials && product.socials.length > 0" class="py-7 md:py-10">
@@ -109,7 +116,17 @@
     padding-left: 1.5rem;
   }
 
+  :deep(.detail-advantage ul) {
+    list-style-type: disc;
+    padding-left: 1.5rem;
+  }
+
   :deep(.detail-description ol) {
+    list-style-type: decimal;
+    padding-left: 1.5rem;
+  }
+
+  :deep(.detail-advantage ol) {
     list-style-type: decimal;
     padding-left: 1.5rem;
   }
